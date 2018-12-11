7ecbf179c18a8cd8f84a9b9c80f550b296622c48
QUDUSKUNLE <quduskunle@gmail.com>
2018-12-11 04:00:06 +0100

    Chore(set-up): set up folder structure add .env add README.md set up server

---
+++ src/server/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import expressValidator from 'express-validator';
+import errorHandler from 'errorhandler';
+
+// import routes from './routes/index';
+
+dotenv.config();
+
+
+const prod = process.env.PRODUCTION;
+const port = process.env.PORT;
+
+// initialize application
+const app = express();
+
+// configure application
+app.use(cors());
+app.use(require('morgan')('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+
+
+// app.use('/api/v1', routes);
+app.get('/', (req, res) => res.status(200).json({ home: 'LMS Home' }));
+
+app.listen(port, () => console.log(`LMS running on PORT ${port}`));
+
+module.exports = app;
